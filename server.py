9ae064a23b71fd8e85c7847145ba5301719b7a98
Peter Timoshin <p.timoshin2009@yandex.ru>
2026-01-17 22:45:05 +0300

    Improved video codecs extraction

--- server.py
+++ server.py
@@ -853,12 +853,20 @@ def handle_topic(topic_id: int):
         tracker = RutrackerApi.Rutracker(config.RUTRACKER_USERNAME, config.RUTRACKER_PASSWORD, "https://rutracker.org/")
     magnet = tracker.get_magnet_link(topic_id)
     description = tracker.get_info(topic_id)
-    video_codecs = re.findall(r"(?:Формат\s+)?[Вв]идео:\s*(.+)", description)
+    #print(description)
+    video_codecs = re.findall(r"(?:Формат\s+)?[Вв]идео\s*:\s*(.+)", description)
     audio_tracks = re.findall(r"(Аудио\s*\d*:\s*(.+))", description)
     if len(video_codecs) > 0:
-        video_codecs = video_codecs[-1]
+        video_codecs = video_codecs[-1].replace("\n", "").strip()
+        match = re.match(r'^(\S+)(\s+)?', video_codecs)
+        if match:
+            first_word = match.group(1)
+            if re.search(r'[\u0400-\u04FF\u0500-\u052F]', first_word):
+                # Remove the first word and any following whitespace
+                video_codecs = video_codecs[len(match.group(0)):]
+        
     if len(audio_tracks) > 0:
-        audio_tracks = "<br>".join([item[0] for item in audio_tracks])
+        audio_tracks = "<br>".join([item[0].replace("\n", "").strip() for item in audio_tracks])
     streams = subprocess.run(f'API_PASSWORD="myapipassword" htorrent info -m="{magnet}"', shell=True, capture_output=True).stdout.decode()
     result = []
     lines = streams.strip().split('\n')
